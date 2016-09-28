884e1270e8d5c9764e61a4f6b3fd547402c545d5
El Diablo <El Diablo>
2016-09-28 23:21:18 +0200

    Initial commit

---
+++ app/src/androidTest/java/a666/plectre/com/trackom/ApplicationTest.java
@@ -0,0 +1,13 @@
+package a666.plectre.com.trackom;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/a666/plectre/com/trackom/MainActivity.java
@@ -0,0 +1,13 @@
+package a666.plectre.com.trackom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
